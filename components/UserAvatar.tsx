48576cb4d4b9b011890f8193b3330860de8331d9
lotfimay <lotfimayouf09@gmail.com>
2024-02-20 22:50:59 +0100

    fixed user icon (auth done)

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+
+import userIcon from '@/public/user-icon.png'
 
 function UserAvatar({ userImage }: { userImage?: string }) {
   return (
     <Avatar>
       <AvatarImage src={userImage} />
-      <AvatarFallback>User img</AvatarFallback>
+      <AvatarFallback>
+        <Image width={20} height={20} alt="icon" src={userIcon}/>
+      </AvatarFallback>
     </Avatar>
   );
 }
