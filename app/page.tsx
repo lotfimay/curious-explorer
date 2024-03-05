c9998dedeb3cb85647ac091b4498f1d6a65f1ab1
lotfimay <lotfimayouf09@gmail.com>
2024-03-06 00:34:20 +0100

    added BlogList and Blog components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
+import BlogList from "@/components/BlogList";
 import Categories from "@/components/Categories";
 import Featured from "@/components/Featured";
 export default function Home() {
+  const data = [
+    {
+      id: 1,
+      user: "lotfi_may",
+      title: "My journey as a foreign student in France",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae et magni, eum deserunt veniam at, asperiores quaerat eius sint quos aut quas sed. Fuga doloribus dicta rem vel similique praesentium! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae et magni, eum deserunt veniam at, asperiores quaerat eius sint quos aut quas sed. Fuga doloribus dicta rem vel similique praesentium!",
+      posted_at: Date.now(),
+    },
+    {
+      id: 2,
+      user: "lotfi_may",
+      title: "My journey as a foreign student in France",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae et magni, eum deserunt veniam at, asperiores quaerat eius sint quos aut quas sed. Fuga doloribus dicta rem vel similique praesentium! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae et magni, eum deserunt veniam at, asperiores quaerat eius sint quos aut quas sed. Fuga doloribus dicta rem vel similique praesentium!",
+      posted_at: Date.now(),
+    },
+    {
+      id: 3,
+      user: "lotfi_may",
+      title: "My journey as a foreign student in France",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae et magni, eum deserunt veniam at, asperiores quaerat eius sint quos aut quas sed. Fuga doloribus dicta rem vel similique praesentium! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae et magni, eum deserunt veniam at, asperiores quaerat eius sint quos aut quas sed. Fuga doloribus dicta rem vel similique praesentium!",
+      posted_at: Date.now(),
+    },
+    {
+      id: 4,
+      user: "lotfi_may",
+      title: "My journey as a foreign student in France",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae et magni, eum deserunt veniam at, asperiores quaerat eius sint quos aut quas sed. Fuga doloribus dicta rem vel similique praesentium! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae et magni, eum deserunt veniam at, asperiores quaerat eius sint quos aut quas sed. Fuga doloribus dicta rem vel similique praesentium!",
+    },
+  ];
+
   return (
     <>
       <Featured />
       <Categories className="mt-4" />
+      <BlogList className="mt-4 bg-red-500" blogs={data} />
     </>
   );
 }
---
+++ components/BlogList.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
+
+interface BlogProps {
+  id: Number;
+  user: string;
+  userImg?: string;
+  title: string;
+  description: string;
+  posted_at?: Number;
+  className?: string;
+}
+export const Blog = ({
+  title,
+  user,
+  userImg,
+  description,
+  posted_at,
+  className,
+}: BlogProps) => {
+  return (
+    <div className={`flex items-center gap-5 w-[70%] ${className}`}>
+      <div className="relative rounded-md h-[200px] w-[300px]">
+        <Image src="/p1.jpeg" alt="" fill className="object-cover rounded-md" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <div>
+          <h1 className="text-xl font-extrabold">{title}</h1>
+          <p className="text-xs text-soft-foreground">
+            posted by <span className="font-extrabold">@{user}</span>, At
+            January 3rd , 2024
+          </p>
+        </div>
+        <p className="text-soft-foreground">
+          {description.substring(0, 200)} ...
+        </p>
+        <Button className="w-fit">Read more</Button>
+      </div>
+    </div>
+  );
+};
+
+interface BlogListProps {
+  className?: string;
+  blogs: BlogProps[];
+}
+
+function BlogList({ blogs, className }: BlogListProps) {
+  return (
+    <ul className={`${className} flex flex-col gap-2`}>
+      {blogs && blogs.map((blog) => <Blog {...blog} />)}
+    </ul>
+  );
+}
+
+export default BlogList;
