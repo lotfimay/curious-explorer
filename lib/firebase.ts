de44146f36d7b0079682cfba8c6e534e8dfb370f
lotfimay <lotfimayouf09@gmail.com>
2024-03-18 18:16:31 +0100

    added firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMQrg56NcJf6xMCDpEE3ekftwVzhWxohA",
+  authDomain: "curious-explorer-f01f6.firebaseapp.com",
+  projectId: "curious-explorer-f01f6",
+  storageBucket: "curious-explorer-f01f6.appspot.com",
+  messagingSenderId: "288602137284",
+  appId: "1:288602137284:web:25ab53ad3b081e0181df91"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
