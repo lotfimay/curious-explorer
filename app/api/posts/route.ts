49e1bcf6abb35184ef9099a8700831366a2bb77b
Lotfi Mayouf <lotfimayouf09@gmail.com>
2024-07-30 20:21:06 +0200

    added post api route

---
+++ app/api/posts/route.ts
@@ -0,0 +1,62 @@
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+
+  const page = searchParams.get("page");
+  const cat = searchParams.get("cat");
+
+  const POST_PER_PAGE = 3;
+
+  const query = {
+    take: POST_PER_PAGE,
+    skip: (Number(page) - 1) * POST_PER_PAGE,
+    where: {
+      ...(cat && { categoryTitle: cat }),
+    },
+  };
+
+  try {
+    const [posts, count] = await db.$transaction([
+      db.post.findMany(query),
+      db.post.count({ where: query.where }),
+    ]);
+    return new NextResponse(JSON.stringify({ posts, count }), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong" }),
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "User not authenticated" }),
+      { status: 401 }
+    );
+  }
+
+  try {
+    const email = session.user?.email || "";
+    const data = await req.json();
+    const post = await db.post.create({
+      data: {
+        userEmail: email,
+        ...data,
+      },
+    });
+    return new NextResponse(JSON.stringify(post), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong" }),
+      { status: 500 }
+    );
+  }
+};
