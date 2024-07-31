d9b4e7e7e114062d037cef5419829c81eef839b2
Lotfi Mayouf <lotfimayouf09@gmail.com>
2024-07-31 17:04:22 +0200

    export the get method of the categories endpoint

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-const GET = async () => {
+export const GET = async () => {
   try {
     const categories = await db.category.findMany();
     return new NextResponse(JSON.stringify(categories), { status: 200 });
