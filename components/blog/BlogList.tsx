722f3a0bb61c3a188dfb49abceb2e7ebbab735fb
Lotfi Mayouf <lotfimayouf09@gmail.com>
2024-08-05 18:25:22 +0200

    changed api call with db query since it's a server component

--- components/blog/BlogList.tsx
+++ components/blog/BlogList.tsx
@@ -1,23 +1,41 @@
 import React from "react";
-
-import { baseUrl } from "@/constants";
 import Pagination from "./Pagination";
 import { POST_PER_PAGE } from "@/app/api/posts/route";
 import { Blog } from "./Blog";
+import { db } from "@/lib/db";
 
 const getBlogs = async (page: number, cat?: string) => {
+  cat = cat || "";
+  page = page || 1;
   try {
-    const response = await fetch(
-      `${baseUrl}/api/posts?page=${page}&cat=${cat || ""}`,
-      {
-        method: "GET",
-        cache: "no-cache",
-      }
-    );
-    const data = await response.json();
-    return data;
+    const query = {
+      take: POST_PER_PAGE,
+      skip: (page - 1) * POST_PER_PAGE,
+      where: {
+        ...(cat && { categoryTitle: cat }),
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            image: true,
+            name: true,
+          },
+        },
+      },
+    };
+    const [posts, count] = await db.$transaction([
+      db.post.findMany({
+        ...query,
+        orderBy: {
+          posted_at: "desc",
+        },
+      }),
+      db.post.count({ where: query.where }),
+    ]);
+    return { posts, count };
   } catch (error) {
-    return Error("Something went wrong");
+    console.log("Somethin went wrong");
   }
 };
 
@@ -28,7 +46,10 @@ interface BlogListProps {
 }
 
 async function BlogList({ page, category, className }: BlogListProps) {
-  const { posts: blogs, count } = await getBlogs(page, category);
+  const data = await getBlogs(page, category);
+
+  const posts = data?.posts || [];
+  const count = data?.count || 0;
 
   const hasNext = Number(page) * POST_PER_PAGE < count;
   const hasPrevious = Number(page) > 1;
@@ -36,8 +57,8 @@ async function BlogList({ page, category, className }: BlogListProps) {
     <div className={`${className}`}>
       <Pagination hasNext={hasNext} hasPrevious={hasPrevious} page={page}>
         <ul className={`flex flex-col gap-2`}>
-          {blogs &&
-            blogs.map((blog: any) => (
+          {posts &&
+            posts.map((blog: any) => (
               <Blog
                 id={blog.id}
                 title={blog.title}
